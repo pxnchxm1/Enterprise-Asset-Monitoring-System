3a945acd1d1c05311940bac567781ae1ca0bb52e
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2025-05-26 11:54:41 +0530

    [panchami] - Added : initial project structure

---
+++ src/main/java/eams/EnterpriseAssetMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package eams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAssetMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAssetMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eams/EnterpriseAssetMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package eams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAssetMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
