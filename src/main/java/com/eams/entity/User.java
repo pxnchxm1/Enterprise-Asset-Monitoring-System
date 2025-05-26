2702164dff3981ec03882c51ed2032d00dcd34b7
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2025-05-26 19:57:27 +0530

    [panchami]- Refactor : editer field name user_id

--- src/main/java/com/eams/entity/User.java
+++ src/main/java/com/eams/entity/User.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long user_id;
 	private String name;
 	private String email;
 	private String password;
