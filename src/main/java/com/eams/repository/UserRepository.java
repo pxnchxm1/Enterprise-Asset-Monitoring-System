f53c271924b0adbaaab6131ffc9070dfc4100136
sasaanka <sasanka2629@gmail.com>
2025-05-27 15:11:29 +0530

    [Sasanka] Added: User controller,Repository,Mapper,Service,DTO

--- src/main/java/com/eams/repository/UserRepository.java
+++ src/main/java/com/eams/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.eams.repository;
 
-public class UserRepository {
+import com.eams.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
