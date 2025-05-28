755163fa35009947f09f787d9e48706337feb7ca
MudaliarSaurabh <saurabhmudaliar2004@gmail.com>
2025-05-28 15:02:06 +0530

    [Saurabh] - Added: Validation

--- src/main/java/com/eams/entity/MaintenanceLog.java
+++ src/main/java/com/eams/entity/MaintenanceLog.java
@@ -1,5 +1,7 @@
 package com.eams.entity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,9 +22,11 @@ public class MaintenanceLog {
 
     @Column(name = "asset_id", nullable = false)
     private Long asset_id; 
+    
+    @NotNull(message = "Date cannot be blank")
     private LocalDate scheduledDate;
-
+    @NotNull(message = "Date cannot be blank")
     private LocalDate completedDate;
-
+    @NotBlank(message = "Remarks cannot be null")
     private String remarks;
 }
