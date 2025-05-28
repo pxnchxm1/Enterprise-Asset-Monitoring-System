f75eb3e0fd99c69d441d0f0603c4b9c36a296c5f
sasaanka <sasanka2629@gmail.com>
2025-05-28 15:49:48 +0530

    [Sasanka] Added: Validation for Password

--- src/main/java/com/eams/dtos/UserDTO.java
+++ src/main/java/com/eams/dtos/UserDTO.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,17 +16,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserDTO {
 	
-	@NotBlank(message="user id cannot be null")
+	@NotNull(message="user id cannot be null")
 	private Long user_id;
 	
 	@NotNull(message="name cannot be null")
 	private String name;
 	
 	@NotNull(message="email cannot be null")
-	@Email(message="email should be valid")
+	@Email(message="Email should be valid")
 	private String email;
 	
-	@NotNull(message="Role cannot be null")
+	@Pattern(regexp="^(?=.[a-z])(?=.[A-Z])(?=.[0-9](?=.[!@#$%^&*.]{8,}$))",message="Give a strong and valid passsword ! ")
+	@NotNull(message="Password should not be null")
+	private String password;
+	
 	@Enumerated(EnumType.STRING)
 	private Role role;
 }
