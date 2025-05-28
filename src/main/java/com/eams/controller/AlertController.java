672c7ac9b35a7ebf9f676a4a54b1d766b7210a0a
akharikesh <akharikesh@gmail.com>
2025-05-28 16:50:28 +0530

    [Harikesh] Refactor : Fixed Validation

--- src/main/java/com/eams/controller/AlertController.java
+++ src/main/java/com/eams/controller/AlertController.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.eams.entity.Alert;
 import com.eams.service.AlertService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/alerts")
+@Validated
 public class AlertController {
 	@Autowired
 	private AlertService service;
@@ -24,7 +28,7 @@ public class AlertController {
 	}
 	//get assset id ,type of alert and message
 	@PostMapping("/send-alert")
-	public ResponseEntity<Alert> createAlert(@RequestBody Alert alert){
+	public ResponseEntity<Alert> createAlert(@Valid@RequestBody Alert alert){
 		return ResponseEntity.ok(service.createAlert(alert.getAsset_id(),alert.getType(),alert.getMessage()));
 	}
 	
