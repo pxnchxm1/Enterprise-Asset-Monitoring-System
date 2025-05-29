e11aaf1a6c05bf077fcc49263082e8a8a21bb1fb
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2025-05-29 11:53:16 +0530

    [panchami] - Refactor : checked endpoint working after exception adding

--- src/main/java/com/eams/service/SensorDataService.java
+++ src/main/java/com/eams/service/SensorDataService.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SensorDataService {
@@ -26,43 +29,56 @@ public class SensorDataService {
     public SensorDataService(SensorDataRepository sensorDataRepository) {
         this.sdr = sensorDataRepository;
     }
-    // get asset_id and store in asset
-    public Optional<SensorData> getSensorDatabyAssetID(Long asset_id) {
-        Asset asset = apr.findById(asset_id).orElseThrow();
-        Optional<SensorData> sensorData = Optional.of(sdr.findAll().stream().filter(data -> data.getAsset_id().equals(asset.getAsset_id())).findFirst().orElseThrow());
-        return sensorData;
+    public List<SensorData> getAllSensorData(){
+    	return sdr.findAll();
     }
-    @SuppressWarnings("finally")
+    // get asset_id and store in asset
+    public List<SensorData> getSensorDatabyAssetID(Long asset_id) {
+    	Asset asset = apr.findById(asset_id)
+    		     .orElseThrow(() -> new NoSuchElementException("Asset not found with ID: " + asset_id));
+
+    	List<SensorData> sensorData = sdr.findAll().stream()
+    		    .filter(data -> data.getAsset_id().equals(asset.getAsset_id())).collect(Collectors.toList());
+
+        return sensorData;
+        
+    }
 	public Boolean sendSensorData(SensorData data){
-        try {
-            SensorData sd = new SensorData();
-            sd.setAsset_id(data.getAsset_id());
-            sd.setPressure(data.getPressure());
-            sd.setTemperature(data.getTemperature());
-            sd.setTimestamp(LocalDateTime.now());
-            sdr.save(sd);
             
-            //To send trigger alerts
-            sd.getSensor_data_id();
-            sd.getPressure();
-            sd.getTemperature();
-            sdr.save(sd);
-            Asset asset = apr.findById(sd.getAsset_id()).orElseThrow();
-            if(asset.getThresholdPressure()<sd.getPressure()) {
-            	alertService.createAlert(asset.getAsset_id(), AlertType.valueOf("PRESSURE_HIGH"), "Pressure is too high , Try with different value.");
-            	throw new InvalidSensorDataException("Invalid Pressure Value !");
-            }else if(asset.getThresholdTemp()<sd.getTemperature()) {
-            	alertService.createAlert(asset.getAsset_id(), AlertType.valueOf("TEMP_HIGH"), "Try entering lower temperature");
-            	throw new InvalidSensorDataException("Invalid Temperature Value !");
-            	
-            }
-            
+		 if(data.getPressure()<0) {
+         	
+         	throw new InvalidSensorDataException("Invalid Pressure Value !");
+         }else if(data.getTemperature()<0) {
+         	
+         	throw new InvalidSensorDataException("Invalid Temperature Value !");
+         	
+         }else {
+        	 SensorData sd = new SensorData();
+             sd.setAsset_id(data.getAsset_id());
+             sd.setPressure(data.getPressure());
+             sd.setTemperature(data.getTemperature());
+             sd.setTimestamp(LocalDateTime.now());
+             sdr.save(sd);
+             
+             //To send trigger alerts
+             sd.getSensor_data_id();
+             sd.getPressure();
+             sd.getTemperature();
+             sdr.save(sd);
+             Asset asset = apr.findById(sd.getAsset_id()).orElseThrow();
+             if(asset.getThresholdPressure()<sd.getPressure()) {
+             	alertService.createAlert(asset.getAsset_id(), AlertType.valueOf("PRESSURE_HIGH"), "Pressure is too high , Try with different value.");
+        
+             }else if(asset.getThresholdTemp()<sd.getTemperature()) {
+             	alertService.createAlert(asset.getAsset_id(), AlertType.valueOf("TEMP_HIGH"), "Try entering lower temperature");	
+             	
+             }
+        	 
+         }
+           
+           
+    
             return true;
-        }catch (Exception e){
-            throw new InvalidSensorDataException("This SensorData cannot be Sent ! Try Entering a new value ");
-        }
-        finally {
-return false;
-        }
+       
     }
 }
