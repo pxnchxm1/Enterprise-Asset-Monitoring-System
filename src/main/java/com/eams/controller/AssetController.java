5780c245172834d8ebbb9de66107be2299e56723
SaiSiddartha0906 <saisiddartha09062004@gmail.com>
2025-05-29 17:22:30 +0530

    [Siddartha]:Added:Added log in asset controller

--- src/main/java/com/eams/controller/AssetController.java
+++ src/main/java/com/eams/controller/AssetController.java
@@ -11,7 +11,9 @@ import com.eams.entity.Asset;
 import com.eams.service.AssetService;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/assets")
 
@@ -23,29 +25,33 @@ public class AssetController {
 //This method is to creating new asset
     @PostMapping
     public boolean create(@Valid @RequestBody AssetDTO dto) {
+    	log.info("New asset created sucessfully");
         return assetService.createAsset(dto);
     }
 //This method is to get details of assets
     @GetMapping
     public List<Asset> getAll() {
+    	log.info("get all asset details");
         return assetService.getAllAssets();
     }
 //This method is to get details by ID
     @GetMapping("/{id}")
     public Asset getById(@PathVariable Long id) {
+    	log.info("get asset details by ID");
         return assetService.getAssetById(id);
     }
 
-//This method is is edit the details
+//This method is to edit the details
     @PutMapping("/{id}")
     public String update(@PathVariable Long id, @Valid @RequestBody AssetDTO dto) {
-
+    	log.info("edit the values for asset");
         return assetService.updateAsset(id, dto);
     }
 
 //This method is to delete to the asset by ID
     @DeleteMapping("/{id}/user/{userid}")
     public String delete(@PathVariable Long id,@PathVariable Long userid) {
+    	log.info("delete the asset by using ID");
         return assetService.deleteAsset(id,userid);
     }
 }
