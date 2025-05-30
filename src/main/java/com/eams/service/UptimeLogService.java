2a695ece9c4be84cf8f8011938eabf64509b7d8c
98601700+Shrinath-M@users.noreply.github.com <98601700+Shrinath-M@users.noreply.github.com>
2025-05-30 16:21:44 +0530

    [Shrinath M] Added : Scheduled for Uptime Calculations

--- src/main/java/com/eams/service/UptimeLogService.java
+++ src/main/java/com/eams/service/UptimeLogService.java
@@ -5,11 +5,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.eams.entity.Alert;
+import com.eams.entity.AlertStatus;
 import com.eams.entity.Asset;
+import com.eams.entity.UpTimeLogStatus;
 import com.eams.entity.UptimeLog;
 import com.eams.exception.AssetNotFoundException;
+import com.eams.repository.AlertRepository;
 import com.eams.repository.AssetRepository;
 import com.eams.repository.UptimeLogRepository;
 
@@ -21,19 +26,36 @@ public class UptimeLogService implements UptimeLogServiceInterface {
 	@Autowired   
 	private UptimeLogRepository repo;
 	
+	@Autowired
+	private AlertRepository alertRepo;
+	
 	//Dependency Injection - Automatically injects objects of AssetRepository
 	@Autowired 
 	private AssetRepository assetRepo;
 	
-	public UptimeLog createlog(Long asset_id, UptimeLog log) {
-			Asset asset = assetRepo.findById(asset_id)
-					.orElseThrow(() -> new AssetNotFoundException("Asset with ID " + asset_id + " not found"));
-			UptimeLog newlog = new UptimeLog();
-			newlog.setAsset(asset);
-			newlog.setEndTime(log.getEndTime());
-			newlog.setStartTime(LocalDateTime.now());
-			newlog.setUptimeLogStatus(log.getUptimeLogStatus());
-			return repo.save(newlog);
+	
+	public void methodToScheduleUptimeLogs() {
+		
+	}
+	@Scheduled(fixedRate=10000)
+	public void createlog() {
+			List<Alert> alerts = alertRepo.findAll();
+			for(Alert al : alerts) {
+				Long asset_id = al.getAsset_id();
+				Asset asset = assetRepo.findById(asset_id).orElseThrow(
+						()->  new AssetNotFoundException("Asset not found !! "));
+				UptimeLog newlog = new UptimeLog();
+				newlog.setAsset(asset);
+				newlog.setStartTime(LocalDateTime.now());
+				newlog.setEndTime(LocalDateTime.now().plusSeconds(10));
+				
+				if(al.getStatus()==AlertStatus.ACTIVE) {
+					newlog.setUptimeLogStatus(UpTimeLogStatus.DOWN);
+				}
+				if(al.getStatus()==AlertStatus.RESOLVED)
+					newlog.setUptimeLogStatus(UpTimeLogStatus.UP);
+				repo.save(newlog);		
+			}
 		
 	}
 	public List<UptimeLog> getLogs(Long asset_id){
